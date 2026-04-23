52fffda0774b2bc410b13b9061aa402ed72c3f42
Kishore <kishorekishore4814@gmail.com>
2026-04-23 14:21:06 +0530

    created a new vector search index in mongodb atlas

--- server/models/Student.js
+++ server/models/Student.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcryptjs'
-
 const projectSchema = new mongoose.Schema({
   name:        { type: String, required: true },
   description: { type: String },
